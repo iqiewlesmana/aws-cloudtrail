276a1fbdf2397da0bd33ddf310e2da82e1a90529
iqiewlesmana <iqiewlesmana@gmail.com>
2020-02-17 11:35:37 +0700

    [rev2] 1. Change start date to 5 days ago 2. Add information max results 3. Joining output json from two awscli command 4. Remove accessKeyId select in function filterby_ipaddress 5. Improve output in function lookupby_resourcetype

--- cloudtrail.sh
+++ cloudtrail.sh
@@ -10,8 +10,9 @@
 # - Resource type ( ResourceType )
 # - Resource name ( ResourceName )
 
-START=$(date --date="2 day ago" +"%Y-%m-%dT%H:%M:%SZ")
-MAX_RESULTS=5
+START=$(date --date="5 day ago" +"%Y-%m-%dT%H:%M:%SZ")
+
+MAX_RESULTS=1
 echo "List of All Event Name with following information :
  - AWS Access Key used in an event ( AccessKeyId )
  - Event time ( EventTime )
@@ -22,32 +23,31 @@ echo "List of All Event Name with following information :
  - Resource type ( ResourceType )
  - Resource name ( ResourceName )
 "
-echo "Pulling cloudtrail from a 2 days ago ($START)"
+echo "Pulling cloudtrail from ($START)"
+echo "max results "$MAX_RESULTS""
 
 lookup_eventname () {
   #Return certains information
   read -p "Enter AttributeKey :" Akey
   read -p "Enter AttributeValue :" Avalue
   echo "Looking for Access Key used in event : $Avalue"
-  aws cloudtrail lookup-events --lookup-attributes  AttributeKey="$Akey",AttributeValue="$Avalue" --max-results "$MAX_RESULTS" --query 'Events[].{username:Username,EventTime:EventTime,eventName:EventName,accesskey:AccessKeyId,resourceName:(Resources[0].ResourceName),resourceType:(Resources[0].ResourceType)}' --output table --start-time "$START"
-
+  aws cloudtrail lookup-events --lookup-attributes AttributeKey="$Akey",AttributeValue="$Avalue"  --start-time "$START" --output json | jq -r '.Events | .[]' | jq '{username: .Username,Event_Time: .EventTime | strftime("%F %T"),Event_Name: .EventName, Access_Key: .AccessKeyId,resource_Name: .Resources[0]}'
+  aws cloudtrail lookup-events --lookup-attributes AttributeKey="$Akey",AttributeValue="$Avalue"  --start-time "$START" --output json | jq -r '.Events | .[] | .CloudTrailEvent' | jq '{Region: .awsRegion,IP_Address:.sourceIPAddress,Event_Name:.eventName}' | jq -s 'add'
 }
 
-
 filterby_ipaddress () {
-  #Filter the results by sourceIPAddress & AccessKeyId
+  #Filter event results by sourceIPAddress
   read -p "Enter to Filter by source IP Address :" ipaddress
-  read -p "Enter to Filter by Access Key ID :" accesskeyid
   read -p "Enter AttributeValue :" Avalue
-  echo "Filter All Event Name $Avalue By Access Key ID $accesskeyid & with source IP Address $ipaddress"
-  aws cloudtrail lookup-events --lookup-attributes  AttributeKey=EventName,AttributeValue="$Avalue" --start-time "$START" --max-results "$MAX_RESULTS" --output json | jq -r '.Events[] | .CloudTrailEvent' | jq -r 'select(if ((.userIdentity.accessKeyId | startswith("'$accesskeyid'")) and (.sourceIPAddress | startswith("'$ipaddress'"))) then false else true end)'
+  echo "Filter All Event Name with source IP Address $ipaddress"
+  aws cloudtrail lookup-events --lookup-attributes AttributeKey=EventName,AttributeValue="$Avalue" --start-time "$START" --max-results "$MAX_RESULTS" --output json | jq -r '.Events[] | .CloudTrailEvent' | jq -r 'select(.sourceIPAddress == "'$ipaddress'")'
 }
 
 lookupby_resourcetype () {
   #Ability to get the list of event by resource type
   read -p "Enter AttributeValue :" Avalue
   echo "List of All Event By Resource Type : $Avalue"
-  aws cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceType,AttributeValue="$Avalue" --max-results "$MAX_RESULTS" --query 'Events[].{Event_Name:EventName,Resource_Type:(Resources[0].ResourceType)}' --output table --start-time "$START"
-
+  aws cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceType,AttributeValue="$Avalue" --max-results "$MAX_RESULTS" --output json --start-time "$START" | jq -r '.Events[]' | jq '{Event_Name: .EventName,Resource_Type: .Resources[]}'
 }
 
+$1 $2 $3
